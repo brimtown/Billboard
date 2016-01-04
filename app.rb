f0a5909ee5fb61424a62403c58ea769d9077e5f4
Tim Brown <brown.tim.lee@gmail.com>
2016-01-04 17:49:37 -0600

    imports one file into database

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 require 'active_record'
+require 'csv'
+
+PATH_TO_DATA = './billboard/'
+PATH_TO_TEST = './billboard/1950.csv'
 
 def establish_db_connection
   ActiveRecord::Base.establish_connection(
@@ -31,5 +35,25 @@ def migrate_db
   CreateChartSinglesTable.migrate(:up)
 end
 
+def import_csv(table)
+  table.each do |row|
+    single = ChartSingle.new
+    single.title = row[2]
+    single.artist = row[1]
+    single.position = row[0]
+    single.year = 1950
+
+    single.save
+  end
+end
+
+def import_data
+  table = CSV.read(PATH_TO_TEST)[1 .. -1]
+  #puts "table"
+  #puts table
+  import_csv table
+end
+
 establish_db_connection
-migrate_db
+#migrate_db
+import_data
